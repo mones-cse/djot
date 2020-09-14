fc5aa10e64b9b52684725563731e349a797e1455
Mones <mones.cse@gmail.com>
2020-09-15 02:35:00 +0600

    added quesiton and choice in admin panel

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Question, Choice
 # Register your models here.
+class QuestionAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice, QuestionAdmin)
