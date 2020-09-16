8256d91c8ab1d21a2ba1e37dad92cdc63f120953
Mones <mones.cse@gmail.com>
2020-09-16 23:58:12 +0600

    add details result vote view

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -4,3 +4,15 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+
+def details(request):
+    return HttpResponse("Details")
+
+
+def result(request):
+    return HttpResponse("Result")
+
+
+def vote(request):
+    return HttpResponse("Vote")
