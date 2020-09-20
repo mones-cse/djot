fb1b9657b7de8322f13668b99781a65523faec65
Mones <mones.cse@gmail.com>
2020-09-20 14:26:53 +0600

    use HttpResponse to show all questions in index view

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Question
+
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    q = Question.objects.all()
+    question_list = []
+    for each in q:
+        question_list.append(each.question_text)
+    question_list_text = ', '.join(question_list)
+    return HttpResponse(question_list_text)
 
 
 def details(request, question_id):
