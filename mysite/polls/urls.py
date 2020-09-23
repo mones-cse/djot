bf1ae6283c80b5e361fa95b9f383f79d968d9e52
Mones <mones.cse@gmail.com>
2020-09-23 20:21:40 +0600

    reverse & httpResponseRedirect

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -5,6 +5,6 @@ app_name = "polls"
 urlpatterns = [
     path('', views.index),
     path('<int:question_id>/', views.details, name='details'),
-    path('<int:question_id>/result/', views.result),
-    path('<int:question_id>/vote/', views.vote)
+    path('<int:question_id>/result/', views.result, name='result'),
+    path('<int:question_id>/vote/', views.vote, name='vote')
 ]
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,6 +1,7 @@
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, Http404 ,HttpResponseRedirect
 from django.template import loader
 from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
 
 from .models import Question, Choice
 
@@ -22,8 +23,32 @@ def details(request, question_id):
 
 
 def result(request, question_id):
-    return HttpResponse("Result of %s" % question_id)
+    question = get_object_or_404(Question,pk=question_id)
+    return render(request, 'polls/result.html', {'question': question})
 
 
 def vote(request, question_id):
-    return HttpResponse("Vote of %s" % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except:
+        return render(request,
+                      'polls/detail.html',
+                      {'selected_question': question,
+                       'error_message': 'Dude you need to choose something!!!'}
+                      )
+    else:
+        selected_choice.votes = selected_choice.votes + 1
+        selected_choice.save()
+        print(reverse('polls:result', args=(question_id,)))
+        return HttpResponseRedirect( reverse('polls:result', args=(question_id,)))
+
+
+# def votes(request, question_id):
+#     try:
+#         selected_choice = get_object_or_404(Question, pk=question_id).choice_set.get(pk=request.POST['choice'])
+#     except:
+#         raise Http404("Question does not exist")
+#     else:
+#
+#         return render(request,'polls/vote.html',{'selected_choice':selected_choice})
