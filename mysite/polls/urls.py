272beee6f7fc4f360391f922675f2413d5dbe469
Mones <mones.cse@gmail.com>
2020-09-23 16:23:45 +0600

    remove hardcoded url

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
-    path('<int:question_id>/', views.details),
+    path('<int:question_id>/', views.details, name='details'),
     path('<int:question_id>/result/', views.result),
     path('<int:question_id>/vote/', views.vote)
 ]
